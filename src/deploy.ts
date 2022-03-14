9def8e931cadc23c3e887d9ce9c4f4155632fb0a
Aseem Sood <aseem@Aseems-MacBook-Pro.local>
2022-03-14 12:48:00 -0400

    add 1bp fee tier

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,6 +2,7 @@ import { Signer } from '@ethersproject/abstract-signer'
 import { BigNumber } from '@ethersproject/bignumber'
 import { migrate } from './migrate'
 import { MigrationState, MigrationStep, StepOutput } from './migrations'
+import { ADD_1BP_FEE_TIER } from './steps/add-1bp-fee-tier'
 import { DEPLOY_MULTICALL2 } from './steps/deploy-multicall2'
 import { DEPLOY_NFT_DESCRIPTOR_LIBRARY_V1_3_0 } from './steps/deploy-nft-descriptor-library-v1_3_0'
 import { DEPLOY_NFT_POSITION_DESCRIPTOR_V1_3_0 } from './steps/deploy-nft-position-descriptor-v1_3_0'
@@ -20,6 +21,7 @@ import { TRANSFER_V3_CORE_FACTORY_OWNER } from './steps/transfer-v3-core-factory
 const MIGRATION_STEPS: MigrationStep[] = [
   // must come first, for address calculations
   DEPLOY_V3_CORE_FACTORY,
+  ADD_1BP_FEE_TIER,
   DEPLOY_MULTICALL2,
   DEPLOY_PROXY_ADMIN,
   DEPLOY_TICK_LENS,
